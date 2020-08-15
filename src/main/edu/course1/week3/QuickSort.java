eb2dee2b70ec4d89ca1a90c26f5295602e30a2dc
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2020-08-15 14:27:45 +0200

    read file and implement int array to store data

---
+++ src/main/edu/course1/week3/QuickSort.java
@@ -0,0 +1,13 @@
+package main.edu.course1.week3;
+
+import main.edu.course1.Utils;
+
+
+public class QuickSort {
+    public static final String readFile = ".//src/main/edu/course1/week3/QuickSort.txt";
+
+    public static void main(String[] args) {
+        int[] array = Utils.readInputFile(readFile, 10000);
+
+    }
+}
