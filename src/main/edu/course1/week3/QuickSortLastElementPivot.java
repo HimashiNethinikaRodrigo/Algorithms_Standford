f0d791ae35ed1f04c9e6a1800ed595a955167c11
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2020-08-16 13:37:35 +0200

    QuickSort- select lastElement of array as Pivot

---
+++ src/main/edu/course1/week3/QuickSortLastElementPivot.java
@@ -0,0 +1,12 @@
+package main.edu.course1.week3;
+
+public class QuickSortLastElementPivot {
+    public static int partitionArray(int[] array, int low, int high) {
+        QuickSortGeneral.swap(array, low, high);
+        return QuickSortFirstElementPivot.partitionArray(array, low, high);
+    }
+
+
+}
+
+//Ans: 164123
